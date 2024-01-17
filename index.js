64914cece2cfdc2eb827776a8febe707688a50a9
sahalmahin <samirsatter0142@gmail.com>
2024-01-18 02:57:07 +0600

    read operation of each camp

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/camps/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await campCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
 
