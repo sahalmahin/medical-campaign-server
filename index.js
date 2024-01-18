c55ff5dd27530a59da26366822b651c0d83a39dd
sahalmahin <samirsatter0142@gmail.com>
2024-01-19 03:03:22 +0600

    delete the added camp implemented

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/add-a-camp/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addCampCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
     // jwt related api
