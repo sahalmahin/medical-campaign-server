c8d6b3ca3f6baca50d0560261eb2cce6fde54998
sahalmahin <samirsatter0142@gmail.com>
2024-01-23 02:13:16 +0600

    check the existing user

--- index.js
+++ index.js
@@ -37,10 +37,14 @@ async function run() {
     // users
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
-
 
     // camps
     app.get('/camps', async (req, res) => {
